980e0a669980ad0558e97c6a2a50483214e293dc
Will Gu <willgu29@gmail.com>
2016-05-01 20:37:59 -0700

    Wireframing app

---
+++ Esusu/CodeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CodeViewController.swift
+//  Esusu
+//
+//  Created by William Gu on 5/1/16.
+//  Copyright © 2016 Gu Studios. All rights reserved.
+//
+
+import UIKit
+
+class CodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
